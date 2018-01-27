af06275c49710145156f47dbaf97ef24ed35ebc5
Thiago Almeida Vergani <thiago.vergani@gmail.com>
2018-01-27 20:44:00 -0200

    Tela Inicial Bem no Inicio.

---
+++ Assets/Scripts/02-Title/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		
+		// Activate the menu from the title screen, and revert to the title screen
+		if (Input.anyKeyDown)
+		{
+			Debug.Log("Press Button: " + Input.compositionString );
+			this.goToLobby();
+		}                    
+		
+	}
+
+	public void goToLobby()
+	{
+		SceneManager.LoadScene("03-Lobby");
+	}
+}
