34b259efda27f1ab0934ef929fa69ef2bd5839b1
Rafael Forbeck <rafaelforbeck@gmail.com>
2018-01-28 09:14:44 -0200

    Implementa a tela de game over Implementa o player da música do jogo

---
+++ Assets/Scripts/04-Gameplay/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicPlayer : MonoBehaviour {
+
+    AudioSource audioSource;
+
+    public AudioClip intro;
+    public AudioClip loop;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = true;
+        StartCoroutine(playEngineSound());
+    }
+
+    IEnumerator playEngineSound()
+    {
+        audioSource.clip = intro;
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length);
+        audioSource.clip = loop;
+        audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/GameOver/GameOver.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    SpriteRenderer victoryImage;
+    public Sprite teamAImage;
+    public Sprite teamBImage;
+
+    public float deadTime;
+    private float currentDeadTime;
+
+	void Start () {
+
+        currentDeadTime = deadTime;
+
+        victoryImage = GetComponent<SpriteRenderer>();
+        victoryImage.sprite = true ? teamAImage : teamBImage;
+	}
+	
+	void Update () {
+
+        currentDeadTime -= Time.deltaTime;
+
+        if (currentDeadTime > 0)
+        {
+            return;
+        }
+
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        currentDeadTime = 0;
+    }
+}
